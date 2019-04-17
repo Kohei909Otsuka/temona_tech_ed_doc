d543f378ce1bdb1d6279c2d788776eb5e2f03cb9
康平大塚 <dashengkangping@gmail.com>
2019-04-17 15:58:33 +0900

    差分だけ再計算

--- freshman/persistent_todo/04_clean_version/todo_list.rb
+++ freshman/persistent_todo/04_clean_version/todo_list.rb
@@ -8,7 +8,8 @@ class TodoList
     end
 
     set_next_id
-    set_search_index
+    @search_index = {}
+    @list.each {|todo| index(todo)}
   end
 
   def all
@@ -16,25 +17,30 @@ class TodoList
   end
 
   def append(name)
-    @list << Todo.new(@next_id, name)
-    set_search_index
+    todo = Todo.new(@next_id, name)
+    @list << todo
     set_next_id
+    index(todo)
   end
 
   def update(id, name)
     target_index = find_index_by_id(id)
     return false if target_index.nil?
+
     todo = @list[target_index]
+    unindex(todo)
     todo.name = name
-    set_search_index
+    index(todo)
     return true
   end
 
   def remove(id)
     target_index = find_index_by_id(id)
     return false if target_index.nil?
+
+    todo = @list[target_index]
     @list.delete_at(target_index)
-    set_search_index
+    unindex(todo)
     return true
   end
 
@@ -61,32 +67,25 @@ class TodoList
     end
   end
 
-  def set_search_index
-    # NOTE: searchのために空白区切りの転置indexを先に準備しておく
-    # 例えば下記のような２つのタスクが合った場合
-    #   1. call mom
-    #   2. call dad by Sunday
-    # 次のようなHashを予め検索用に計算しておく(listの中身が変わるごとに再計算が必要)
-    # {
-    #   "call": [1,2],
-    #   "mom": [1],
-    #   "mail": [2],
-    #   "dad": [2],
-    #   "by": [2],
-    #   "sunday": [2]
-    # }
-    tmp_index = {}
-    @list.each do |todo|
-      keywords = todo.name.split
-      keywords.each do |key|
-        if tmp_index[key]
-          tmp_index[key].push(todo.id)
-        else
-          tmp_index[key] = [todo.id]
-        end
+  def index(todo)
+    keywords = todo.name.split
+    keywords.each do |key|
+      if @search_index[key]
+        @search_index[key].push(todo.id)
+      else
+        @search_index[key] = [todo.id]
       end
     end
-    @search_index = tmp_index
+  end
+
+  def unindex(todo)
+    keywords = todo.name.split
+    keywords.each do |key|
+      if @search_index[key]
+        @search_index[key].delete(todo.id)
+        @search_index.delete(key) if @search_index[key].length == 0
+      end
+    end
   end
 
   def find_index_by_id(id)
