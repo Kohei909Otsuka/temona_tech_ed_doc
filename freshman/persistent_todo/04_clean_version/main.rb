85a88838203c6ae269df82fbabe3a8a68f508b44
康平大塚 <dashengkangping@gmail.com>
2019-05-17 19:36:09 +0900

    fix: ruby 2.3.0だとbegin~endで囲わないと例外処理できない問題に対応

--- freshman/persistent_todo/04_clean_version/main.rb
+++ freshman/persistent_todo/04_clean_version/main.rb
@@ -19,45 +19,46 @@ strage = sg.generate
 list = TodoList.new(strage)
 
 loop do
-  puts "enter mode(#{MODES.join(", ")})"
-  mode = gets.chomp
+  begin
+    puts "enter mode(#{MODES.join(", ")})"
+    mode = gets.chomp
 
-  case mode
-  when MODES[0]
-    puts list.all.map {|todo| todo.format}
-  when MODES[1]
-    puts "enter name"
-    list.append(gets.chomp)
-  when MODES[2]
-    puts "enter id to update"
-    id = gets.chomp.to_i
-    puts "enter new name"
-    name = gets.chomp
-    success = list.update(id, name)
-    if !success
-      puts "todo not found for given id"
+    case mode
+    when MODES[0]
+      puts list.all.map {|todo| todo.format}
+    when MODES[1]
+      puts "enter name"
+      list.append(gets.chomp)
+    when MODES[2]
+      puts "enter id to update"
+      id = gets.chomp.to_i
+      puts "enter new name"
+      name = gets.chomp
+      success = list.update(id, name)
+      if !success
+        puts "todo not found for given id"
+        next
+      end
+    when MODES[3]
+      puts "enter id to remove"
+      id = gets.chomp.to_i
+      success = list.remove(id)
+      if !success
+        puts "todo not found for given id"
+        next
+      end
+    when MODES[4]
+      puts "enter keyword to search"
+      keyword = gets.chomp
+      puts "search result ids"
+      puts list.search(keyword)
+    else
+      puts "Warning! mode must be one of #{MODES.join(", ")}"
       next
     end
-  when MODES[3]
-    puts "enter id to remove"
-    id = gets.chomp.to_i
-    success = list.remove(id)
-    if !success
-      puts "todo not found for given id"
-      next
-    end
-  when MODES[4]
-    puts "enter keyword to search"
-    keyword = gets.chomp
-    puts "search result ids"
-    puts list.search(keyword)
-  else
-    puts "Warning! mode must be one of #{MODES.join(", ")}"
-    next
-  end
-
   # ctrl + cで終了したら、listを閉じて永続化
   rescue Interrupt => e
     list.close
     return
+  end
 end
